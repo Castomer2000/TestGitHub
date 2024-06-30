939b456cfa3b17030cdc1a6e30c42b97f2de696b
Castomer2000 <rainofcastomer@gmail.com>
2024-06-30 15:00:09 +0300

    My project

---
+++ src/Human.java
@@ -0,0 +1,14 @@
+public class Human {
+    String name;
+    int age;
+    String work;
+
+    public Human(String name, int age, String work) {
+        this.name = "Vladimir";
+        this.age = 40;
+        this.work = work;
+    }
+    void human(){
+        System.out.println(name + "; " + age + "; " + work + ".");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("my branch");
-    
+        Human human = new Human("Vova", 40, "Russian Railways");
+        human.human();
     }
 }
